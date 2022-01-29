30ba58f013c8aaee90162dc54fbb02de8ef7d7b7
Medardus2004 <tobim.doe@web.de>
2022-01-29 20:06:10 +0100

    faet(bash) add Task 11

--- inProgress/Task011.c
+++ inProgress/Task011.c
@@ -1,35 +1,86 @@
-#include<stdio.h>
-#include <stdbool.h>
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
 
 
-bool isPrime(int checknumber) 
+int main(void)
 {
-	bool Prime = true;
+    int maxproduct;
+    int grid[20][20];
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int i = 0;
+    int j = 0;
 
-	for(int a = 2; a < checknumber; a++)
+    fp = fopen("file_011.txt", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        char * pch;
+        pch = strtok (line," ,.-");
+        j = 0;
+	while (pch != NULL)
 	{
-		if(checknumber%2 == 0)
-		{
-			Prime = false;
-		}
+        	grid[i][j] = strtol(pch, NULL, 10) ;
+        	j++; 
+		pch = strtok (NULL, " ,.-");
+        }
+	i++;
+    }
+
+    	fclose(fp);
+    	if (line)
+        free(line);
+
+	maxproduct = 0;
+
+   for(int column = 0; column < 20; column++)
+     {
+     for(int row = 0; row < 16; row++)
+       {
+         if(grid[column][row] * grid[column][row + 1] * grid[column][row + 2] * grid[column][row + 3] > maxproduct )
+	{        
+		maxproduct = grid[column][row] * grid[column][row + 1] * grid[column][row + 2] * grid[column][row + 3];
+		printf("%i , %i , %i , %i \n", grid[column][row] , grid[column][row + 1] , grid[column][row + 2] , grid[column][row + 3]);
 	}
-
-	return Prime;
-}
-
-void main () {
-
-
-
-for (int a = 1; a < 1000; a++)
-	{	
-		if(isPrime(a))
-					printf("a: %i ,", a);
-				
 	}
+     }
+ for(int row = 0; row < 20; row++)
+     {
+     for(int column = 0; column < 16; column++)
+       {
+         if(grid[column][row] * grid[column+1][row] * grid[column+2][row] * grid[column+3][row] > maxproduct )
+	 {
+        	maxproduct = grid[column][row] * grid[column+1][row] * grid[column+2][row] * grid[column+3][row];
+		printf("%i , %i , %i , %i \n", grid[column][row] , grid[column+1][row] , grid[column+2][row] , grid[column+3][row]);
+         }
+ 	}
+     }
+ for(int row = 0; row < 17; row++)
+     {
+     for(int column = 0; column < 17; column++)
+       {
+	if(grid[column][row] * grid[column+1][row+1] * grid[column+2][row+2] * grid[column+3][row+3] > maxproduct )        
+	maxproduct = grid[column][row] * grid[column+1][row+1] * grid[column+2][row+2] * grid[column+3][row+3];
+        }
+     }
+
+ for(int row = 3; row < 20; row++)
+     {
+     for(int column = 0; column < 17; column++)
+       {
+        if(grid[column][row] * grid[column+1][row-1] * grid[column+2][row-2] * grid[column+3][row-3] > maxproduct )
+          maxproduct = grid[column][row] * grid[column+1][row-1] * grid[column+2][row-2] * grid[column+3][row-3];
+        }
+     }
+
+    printf("The biggest Product of four digits is %i \n", maxproduct );
+
 
 }
-
-
-
